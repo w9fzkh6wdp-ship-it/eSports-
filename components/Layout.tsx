caddeb549cedd7c67fde6af390bc30cc79cade6e
w9fzkh6wdp-ship-it <w9fzkh6wdp@privaterelay.appleid.com>
2026-06-06 00:04:30 +0530

    Add Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode } from 'react';
+import Navigation from './Navigation';
+import Footer from './Footer';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navigation />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
+  );
+}
